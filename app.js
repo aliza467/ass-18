f822a01c092b55e9466f7d3f473fbd66f910cbbd
aliza <ak6614462@gmail.com>
2024-03-24 00:57:34 +0500

    ass18

---
+++ app.js
@@ -0,0 +1,108 @@
+var allLower = userInput.toLowerCase();
+
+x = x.toLowerCase();
+
+y = y.toLowerCase();
+
+var x="aliza"
+var y=aliza.toLowerCase();
+
+var array = ["a"]; 
+var lowerCaseElement = array[0].toLowerCase();
+
+var str="aliza"
+var uppercase=str.toUpperCase();
+alert(uppercase)
+
+var cityName = "kaRacHi";
+var capitalizedCityName = cityName.slice(0,1).toUpperCase() + cityName.slice(1).toLowerCase();
+console.log(capitalizedCityName);
+
+var samewords="captain";
+console.log(samewords.slice(1,3));
+
+var samewords="captain";
+var x=samewords.length;
+console.log(x)
+
+var animal="elephant"
+var seg=animal.slicelice(2,6)
+console.log(seg)
+
+var originalString = "YourStringHere"; 
+var numberOfCharacters = originalString.length;
+var slicedString = originalString.slice(1, -3);
+console.log("Number of characters:", numberOfCharacters);
+console.log("Sliced string:", slicedString);
+
+3
+
+4
+// pdf2
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var fullName = firstName + " " + lastName;
+alert("Hello, " + fullName + "! Welcome!");
+
+
+
+var favoriteModel = prompt("What is your favorite mobile phone model?");
+var inputLength = favoriteModel.length;
+document.write("My favourite phone is:","",favoriteModel,"<br>");
+document.write(inputLength);
+
+
+
+var word = "Pakistani";
+var index = word.indexOf("n");
+document.write("String: ",word,"<br>","Index of n: ",index);
+
+
+
+var word = "Hello World";
+var lastIndex = word.lastIndexOf("l");
+document.write("String: ",word,"<br>","Last index of l ",lastIndex)
+
+
+var word = "Pakistani";
+var characterAtIndex3 = word.charAt(3);
+document.write("String: ",word,"<br>","The character at the 3rd index : " + characterAtIndex3 );
+
+
+
+
+var word = "Hyderabad";
+var replacedWord = word.replace("Hyder", "Islam");
+document.write("City; ",word,"<br>","After replacement: ",replacedword);
+
+
+
+var str = "472";
+var num = +(str);
+document.write("Original String: " + str );
+document.write("Type of Original String: " + typeof str );
+document.write("Converted Number: " + num );
+document.write("Type of Converted Number: " + typeof num );
+
+
+
+var userInput = prompt("Enter your input:");
+var capitalLetters = userInput.toUpperCase();
+document.write("UserInput: ",userInput,"<br>","UPPERCASE: ",capitalLetters)
+
+
+var name=prompt("Enter full name ");
+var titlename = cityName.slice(0,1).toUpperCase() + cityName.slice(1).toLowerCase();
+console.log(titalname);
+
+
+
+var num = 35.36;
+var numAsString = num.toString();
+var numWithoutDot = numAsString.replace(".", "");
+document.write("<h1>Number after removing the dot: " + numWithoutDot + "</h1>");
+
+
+
+
+        
